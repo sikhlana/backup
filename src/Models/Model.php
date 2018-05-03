6e000bb9e967a6c896b85537a0a0574efec9d2f4
Saif M <sikhlana@gmail.com>
2018-05-03 20:26:30 +0600

    Added new helper functions.

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -2,7 +2,7 @@
 
 namespace Sikhlana\Backup\Models;
 
-abstract class Model
+abstract class Model implements \ArrayAccess
 {
     /**
      * @var array
@@ -21,5 +21,74 @@ abstract class Model
         }
 
         return null;
+    }
+
+    public function getAttributes(array $keys)
+    {
+        $return = [];
+
+        foreach ($keys as $key) {
+            if (! is_null($value = $this->getAttribute($key))) {
+                $return[$key] = $value;
+            }
+        }
+
+        return $return;
+    }
+
+    public function setAttribute($key, $value)
+    {
+        $this->attributes[$key] = $value;
+
+        return $this;
+    }
+
+    public function setAttributes(array $values)
+    {
+        foreach ($values as $key => $value) {
+            $this->setAttribute($key, $value);
+        }
+
+        return $this;
+    }
+
+    public function offsetExists($offset)
+    {
+        return ! is_null($this->getAttribute($offset));
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->getAttribute($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->setAttribute($offset, $value);
+    }
+
+    public function offsetUnset($offset)
+    {
+        $this->setAttribute($offset, null);
+    }
+
+    public function __get($name)
+    {
+        return $this->getAttribute($name);
+    }
+
+    public function __set($name, $value)
+    {
+        return $this->setAttribute($name, $value);
+    }
+
+    public function __isset($name)
+    {
+        return $this->offsetExists($name);
+    }
+
+    public function __unset($name)
+    {
+        return $this->offsetUnset($name);
     }
 }
